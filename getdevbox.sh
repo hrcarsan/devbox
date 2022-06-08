c0fc5c2f5a35757905e385e07d506c23299a9e62
Santiago Herrera C <santiago.cardona@inkaviation.com>
2022-06-08 14:52:22 -0500

    wip

--- getdevbox.sh
+++ getdevbox.sh
@@ -4,6 +4,6 @@
 
 wget -O devbox-installer.sh https://raw.githubusercontent.com/hrcarsan/devbox/master/devbox-installer.sh
 bash devbox-installer.sh
-rm devbox-installer.sh
+#rm devbox-installer.sh
 
 } # End of wrapping
