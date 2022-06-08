19382f308eea236aaa014a6edd40a33e11c67697
Santiago Herrera C <santiago.cardona@inkaviation.com>
2022-06-08 15:12:22 -0500

    wip

--- getdevbox.sh
+++ getdevbox.sh
@@ -2,8 +2,8 @@
 
 { # Prevent execution if this script was only partially downloaded
 
-wget -O devbox-installer.sh https://raw.githubusercontent.com/hrcarsan/devbox/master/devbox-installer.sh
+wget -qO devbox-installer.sh https://raw.githubusercontent.com/hrcarsan/devbox/master/devbox-installer.sh
 bash devbox-installer.sh
-#rm devbox-installer.sh
+rm devbox-installer.sh
 
 } # End of wrapping
