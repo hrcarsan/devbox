2983e7d416f9b516a174341c9e601392eecde2ca
Santiago Herrera C <santiago.cardona@inkaviation.com>
2022-06-08 16:45:19 -0500

    wip

--- getdevbox.sh
+++ getdevbox.sh
@@ -4,7 +4,7 @@
 
 wget -H 'Cache-Control: no-cache, no-store' -qO \
   devbox-installer.sh https://raw.githubusercontent.com/hrcarsan/devbox/master/devbox-installer.sh
-bash devbox-installer.sh
+bash -i devbox-installer.sh
 rm devbox-installer.sh
 
 } # End of wrapping
